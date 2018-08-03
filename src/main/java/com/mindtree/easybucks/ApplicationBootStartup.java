fbb132fabe1d826cf132c0ea1c3da9863478d726
M1046249 <M1046249@A2ML26807.mindtree.com>
2018-08-03 17:15:46 +0530

    Test CD

--- src/main/java/com/mindtree/easybucks/ApplicationBootStartup.java
+++ src/main/java/com/mindtree/easybucks/ApplicationBootStartup.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+/**
+ * SpringBootServletInitializer : Required for External Tomcat/WebContaier war deloyment 
+ * @author M1046249
+ *
+ */
 @SpringBootApplication
 public class ApplicationBootStartup extends SpringBootServletInitializer {
 
@@ -12,6 +18,8 @@ public class ApplicationBootStartup extends SpringBootServletInitializer {
 		SpringApplication.run(ApplicationBootStartup.class, args);
 		System.out.println("====>Good <====");
 	}
+	
+	
 	
 	@Override
 	protected SpringApplicationBuilder configure( SpringApplicationBuilder builder) {
