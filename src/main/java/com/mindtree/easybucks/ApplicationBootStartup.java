1d59e95987e3add538682bac32e371fff5e088c0
M1046249 <M1046249@A2ML26807.mindtree.com>
2018-08-03 15:30:13 +0530

    Arey Yaar...Abhi tho kuch

--- src/main/java/com/mindtree/easybucks/ApplicationBootStartup.java
+++ src/main/java/com/mindtree/easybucks/ApplicationBootStartup.java
@@ -2,12 +2,20 @@ package com.mindtree.easybucks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationBootStartup {
+public class ApplicationBootStartup extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationBootStartup.class, args);
 		System.out.println("====>Good <====");
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure( SpringApplicationBuilder builder) {
+		
+		return builder.sources(ApplicationBootStartup.class);
 	}
 }
